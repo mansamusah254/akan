e44aae942891c8e7db235fab50cf699ea8c0e22a
MosesKamotho <mansamusah830@gmail.com>
2022-03-07 09:16:43 +0300

    function and variables for the day

--- node.js
+++ node.js
@@ -9,3 +9,8 @@ var day = [
   ]
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"]
+function dateOfBirth(){
+    var day = document.getElementById("day").value
+    var dd=parseInt(DelayNode)
+
+}
