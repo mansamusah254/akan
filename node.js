82505d6e2073240ab29d3b46f8f8850212028584
MosesKamotho <mansamusah830@gmail.com>
2022-03-07 09:23:18 +0300

    formula for calculating the day

--- node.js
+++ node.js
@@ -15,4 +15,8 @@ function dateOfBirth(){
     var month = document.getElementById("month").value
     var mm=parseInt(month)
     var year=document.getElementById("year").value
+    var yy=parseInt(year.substr(2,4));
+    var cc=parseInt(year.substr(0,2));
+  var day=parseInt(((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10))+ dd)%7;
+  var gender=document.getElementById("gender").value
 }
