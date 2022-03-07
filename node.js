2fd1c00037b54cd0110cc561886cacfb469b42ef
MosesKamotho <mansamusah830@gmail.com>
2022-03-07 09:13:35 +0300

    added variables for the names and days

---
+++ node.js
@@ -0,0 +1,11 @@
+var day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"]
